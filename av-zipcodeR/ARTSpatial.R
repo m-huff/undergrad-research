13029d11ece35661c3edb7736eb6b113ddd0727d
m-huff <michaelhuff17@gmail.com>
2021-09-17 22:21:30 -0700

    Implement ZIP code coordinate pairing

--- av-zipcodeR/ARTSpatial.R
+++ av-zipcodeR/ARTSpatial.R
@@ -6,7 +6,15 @@
 ### appends the ZIP code's centroid latitude and longitude to two columns
 ### immediately after the ZIP code. This allows us to import the ZIP
 ### code data spatially into QGIS and run 'Count Points in Polygon'
-### directly from the raw data.
+### directly from the raw data to get counts of respondents per ZIP code,
+### state, and urban/rural area.
+
+### This package, zipcodeR, does not include zip code data for Hawaii and
+### Alaska, and seems to be missing a couple others, which can be filled in
+### using the FreeMapTools in-browser tool, "Convert USA ZIP Code to 
+### Latitude / Longitude", located at the link below:
+
+### https://www.freemaptools.com/convert-us-zip-code-to-lat-lng.htm
 
 library(zipcodeR)
 survey <- read.csv("C:/Users/micha/Desktop/AV Research/art-dummy.csv")
@@ -16,168 +24,63 @@ survey <- read.csv("C:/Users/micha/Desktop/AV Research/art-dummy.csv")
 # are not valid ZIP codes.
 
 # The reason this is so ugly is because the syntax being
-# incorrect is not the only reason a ZIP code can be invalid.
-# In many cases, the ZIP codes entered are 5-digit numbers,
-# but they have no corresponding jurisdiction in the United
-# States.
+# incorrect is not the only reason a ZIP code can be invalid,
+# and we need to eliminate these records based on whether or
+# not zipcodeR failed at that specific index, hence the list
+# of magic numbers below.
 
-survey <- survey[c(-82),]
-survey <- survey[c(-132),]
-survey <- survey[c(-218),]
-survey <- survey[c(-237),]
-survey <- survey[c(-243),]
-survey <- survey[c(-251),]
-survey <- survey[c(-332),]
-survey <- survey[c(-332),]
-survey <- survey[c(-405),]
-survey <- survey[c(-425),]
-survey <- survey[c(-460),]
-survey <- survey[c(-475),]
-survey <- survey[c(-475),]
-survey <- survey[c(-486),]
-survey <- survey[c(-753),]
-survey <- survey[c(-757),]
-survey <- survey[c(-775),]
-survey <- survey[c(-843),]
-survey <- survey[c(-851),]
-survey <- survey[c(-872),]
-survey <- survey[c(-1034),]
-survey <- survey[c(-1080),]
-survey <- survey[c(-1091),]
-survey <- survey[c(-1102),]
-survey <- survey[c(-1107),]
-survey <- survey[c(-1136),]
-survey <- survey[c(-1155),]
-survey <- survey[c(-1194),]
-survey <- survey[c(-1252),]
-survey <- survey[c(-1334),]
-survey <- survey[c(-1431),]
-survey <- survey[c(-1452),]
-survey <- survey[c(-1462),]
-survey <- survey[c(-1572),]
-survey <- survey[c(-1607),]
-survey <- survey[c(-1647),]
-survey <- survey[c(-1800),]
-survey <- survey[c(-1832),]
-survey <- survey[c(-1907),]
-survey <- survey[c(-1939),]
-survey <- survey[c(-1956),]
-survey <- survey[c(-1984),]
-survey <- survey[c(-1995),]
-survey <- survey[c(-2077),]
-survey <- survey[c(-2096),]
-survey <- survey[c(-2155),]
-survey <- survey[c(-2176),]
-survey <- survey[c(-2198),]
-survey <- survey[c(-2299),]
-survey <- survey[c(-2310),]
-survey <- survey[c(-2312),]
-survey <- survey[c(-2313),]
-survey <- survey[c(-2317),]
-survey <- survey[c(-2318),]
-survey <- survey[c(-2318),]
-survey <- survey[c(-2318),]
-survey <- survey[c(-2319),]
-survey <- survey[c(-2323),]
-survey <- survey[c(-2323),]
-survey <- survey[c(-2323),]
-survey <- survey[c(-2323),]
-survey <- survey[c(-2323),]
-survey <- survey[c(-2324),]
-survey <- survey[c(-2324),]
-survey <- survey[c(-2324),]
-survey <- survey[c(-2325),]
-survey <- survey[c(-2326),]
-survey <- survey[c(-2331),]
-survey <- survey[c(-2331),]
-survey <- survey[c(-2332),]
-survey <- survey[c(-2332),]
-survey <- survey[c(-2332),]
-survey <- survey[c(-2336),]
-survey <- survey[c(-2338),]
-survey <- survey[c(-2338),]
-survey <- survey[c(-2339),]
-survey <- survey[c(-2342),]
-survey <- survey[c(-2346),]
-survey <- survey[c(-2347),]
-survey <- survey[c(-2348),]
-survey <- survey[c(-2355),]
-survey <- survey[c(-2355),]
-survey <- survey[c(-2358),]
-survey <- survey[c(-2359),]
-survey <- survey[c(-2364),]
-survey <- survey[c(-2365),]
-survey <- survey[c(-2367),]
-survey <- survey[c(-2372),]
-survey <- survey[c(-2373),]
-survey <- survey[c(-2378),]
-survey <- survey[c(-2382),]
-survey <- survey[c(-2386),]
-survey <- survey[c(-2386),]
-survey <- survey[c(-2401),]
-survey <- survey[c(-2406),]
-survey <- survey[c(-2417),]
-survey <- survey[c(-2423),]
-survey <- survey[c(-2423),]
-survey <- survey[c(-2423),]
-survey <- survey[c(-2425),]
-survey <- survey[c(-2431),]
-survey <- survey[c(-2434),]
-survey <- survey[c(-2448),]
-survey <- survey[c(-2449),]
-survey <- survey[c(-2450),]
-survey <- survey[c(-2451),]
-survey <- survey[c(-2451),]
-survey <- survey[c(-2452),]
-survey <- survey[c(-2456),]
-survey <- survey[c(-2456),]
-survey <- survey[c(-2457),]
-survey <- survey[c(-2461),]
-survey <- survey[c(-2471),]
-survey <- survey[c(-2481),]
-survey <- survey[c(-2492),]
-survey <- survey[c(-2492),]
-survey <- survey[c(-2493),]
-survey <- survey[c(-2498),]
-survey <- survey[c(-2507),]
-survey <- survey[c(-2507),]
-survey <- survey[c(-2510),]
-survey <- survey[c(-2520),]
-survey <- survey[c(-2521),]
-survey <- survey[c(-115),]
-survey <- survey[c(-167),]
-survey <- survey[c(-169),]
-survey <- survey[c(-216),]
-survey <- survey[c(-216),]
-survey <- survey[c(-246),]
+# Reasons for removal include:
+# 1) Invalid ZIP Code (zipcodeR does not recognize it as existing
+#    within the United States despite it being a 5-digit integer)
+# 2) Blank (user did not input their ZIP code)
+# 3) Wrongly input data (user typed in something that was not
+#    a ZIP code or had major issues that could not be corrected)
+
+# This process omits 174 records from the survey data.
+
+itr_rmv_list <- c(82,132,218,237,243,251,332,332,405,425,460,475,475,486,753,757,775,843,851,872,1034,
+                  1080,1091,1102,1107,1136,1155,1194,1252,1334,1431,1452,1462,1572,1607,1647,1800,1832,
+                  1907,1939,1956,1984,1995,2077,2096,2155,2176,2198,2299,2310,2312,2313,2317,2318,2318,
+                  2318,2319,2323,2323,2323,2323,2323,2324,2324,2324,2325,2326,2331,2331,2332,2332,2332,
+                  2336,2338,2338,2339,2342,2346,2347,2348,2355,2355,2358,2359,2364,2365,2367,2372,2373,
+                  2378,2382,2386,2386,2401,2406,2417,2423,2423,2423,2425,2431,2434,2448,2449,2450,2451,
+                  2451,2452,2456,2456,2457,2461,2471,2481,2492,2492,2493,2498,2507,2507,2510,2520,2521,
+                  115,167,169,216,216,246,248,300,307,326,331,350,381,397,411,438,438,450,469,517,604,
+                  697,846,919,942,997,1009,1113,1114,1118,1298,1376,1402,1624,1634,1680,1769,1818,1834,
+                  1839,1845,1912,1951,1952,2000,2061,2261,2416,2437,2439)
+ 
+for (record in itr_rmv_list){ survey <- survey[-record,] }
 
 # Build ourselves a couple new data frames to splice the 
-# data into the dummy data set.
+# data into the dummy data set, with a column for the ZIP
+# code and two columns for the lat/long of the ZIP code's
+# centroid point.
 
 zip_20 <- data.frame(survey[,20])
 zip_21 <- data.frame(survey[,21])
-zip_20$lat <- NA
-zip_20$lon <- NA
-zip_21$lat <- NA
-zip_21$lon <- NA
+zip_20$centr_lat_20 <- NA
+zip_20$centr_lon_20 <- NA
+zip_21$centr_lat_21 <- NA
+zip_21$centr_lon_21 <- NA
 
 # Create coord pairs for each ZIP in both the 2020 and 2021 sets
 # Again, this works by referencing a big internal table in the
 # zipcodeR package that has a centroid lat/long for each ZIP code,
-# so we're just joining that data to our ZIP code lists.
+# so we're just joining that data to our ZIP code lists for GIS use.
 
-#for (i in seq(1, nrow(zip_20), 1)) { 
-#  zip_20[i,] <- geocode_zip(zip_20$survey...20.[i]) 
-#}
+for (i in seq(1, nrow(zip_20), 1)) { zip_20[i,] <- geocode_zip(zip_20$survey...20.[i]) }
+for (i in seq(1, nrow(zip_21), 1)) { zip_21[i,] <- geocode_zip(zip_21$survey...21.[i]) }
 
-for (i in seq(1, nrow(zip_21), 1)) { 
-  zip_21[i,] <- geocode_zip(zip_21$survey...21.[i]) 
-}
+# Now we splice the data back into the dummy data set.
 
-# Now we splice the data back into the dummy dataset.
-
-#survey <- cbind(survey[,1:19],zip_20,zip_21,survey[,22:ncol(survey)])
+survey <- cbind(survey[,1:19],zip_20,zip_21,survey[,22:ncol(survey)])
                 
 # Write to CSV.
 
+write.csv(survey, "C:/Users/micha/Desktop/AV Research/dummy-centroids.csv")
 
+# Remove unneeded variables from data environment.
+
+rm("zip_20","zip_21","i","itr_rmv_list")
+
+### END OF SCRIPT ###
